6d0d961bfe6d8bb53ec519c1940b565a41e9fc0a
jdmentors <contact@jdmentors.com>
2025-08-13 22:05:23 +0530

    Update blog.route.js

--- backend/src/routes/blog.route.js
+++ backend/src/routes/blog.route.js
@@ -2,28 +2,11 @@ import { Router } from "express";
 import verifyAdmin from "../middlewares/verifyAdmin.js";
 import { createBlog, deleteBlog, editBlog, getABlog, getAllBlogs, updateAvailability } from "../controllers/blog.controller.js";
 import upload from "../utils/multer.js";
-import cors from "cors";
 
 const blogRouter = Router();
 
-const allowedOrigins = ['https://www.jdmentors.com', 'http://localhost:5173'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // allow non-browser requests
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization'],
-};
-
-blogRouter.options('/edit/:blogId', cors(corsOptions));
 blogRouter.post('/create', verifyAdmin, upload.single('image'), createBlog);
-blogRouter.put('/edit/:blogId', verifyAdmin, upload.any(), editBlog);
+blogRouter.put('/edit/:blogId',  editBlog);
 blogRouter.get('/single/:slug', getABlog);
 blogRouter.get('/all', getAllBlogs);
 blogRouter.delete('/delete/:blogId', verifyAdmin, deleteBlog);
