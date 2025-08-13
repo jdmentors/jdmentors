93a50ad35f34a3c4fa63b84b9f339051ad4d38d8
jdmentors <contact@jdmentors.com>
2025-08-13 21:56:10 +0530

    Update blog.route.js

--- backend/src/routes/blog.route.js
+++ backend/src/routes/blog.route.js
@@ -5,6 +5,22 @@ import upload from "../utils/multer.js";
 
 const blogRouter = Router();
 
+const allowedOrigins = ['https://www.jdmentors.com', 'http://localhost:5173'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // allow non-browser requests
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization'],
+};
+
+blogRouter.options('/edit/:blogId', cors(corsOptions));
 blogRouter.post('/create', verifyAdmin, upload.single('image'), createBlog);
 blogRouter.put('/edit/:blogId', verifyAdmin, upload.any(), editBlog);
 blogRouter.get('/single/:slug', getABlog);
